94c818e5dcca8f99837ce1d12bf232aab148cccb
Andrew Ellis <andrew.ellis@mheducation.com>
2017-06-17 13:39:07 -0400

    Set up initial database check-in

---
+++ server/db/index.js
@@ -0,0 +1,7 @@
+let admin = require('firebase-admin');
+let serviceAccount = require('../config/admin.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://the-derelicts.firebaseio.com/'
+});
